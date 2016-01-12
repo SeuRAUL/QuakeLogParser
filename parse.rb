8e8ac6d0bcb6824c95dc41ad5e348d89fb63fa55
SeuRAUL <kenny.is.inmortal@gmail.com>
2016-01-12 16:05:37 -0300

    count kills of each player

--- parse.rb
+++ parse.rb
@@ -13,15 +13,27 @@ data = File.open(logfile)
 #   }
 # }
 game = {total_kills: 0,
-        players: []}
+        players: [],
+        kills: {}}
 
 # data.each do
-(1..100).each do
+(1..200).each do
   kill = data.readline.split(' ')
 
   if kill.include? "Kill:"
     game[:total_kills] += 1
-    game[:players] << kill[5] unless kill[5] == "<world>" || game[:players].include?(kill[5])
+
+    if kill[5] != "<world>"
+
+      # Address players
+      game[:players] << kill[5]   unless game[:players].include?(kill[5])
+      
+      # Count kills
+      !game[:kills].has_key?(kill[5]) ? game[:kills][kill[5]] = 1 : game[:kills][kill[5]] += 1
+
+    end
+    
+    
     game[:players] << kill[7] unless game[:players].include? kill[7]
 
     
@@ -30,7 +42,8 @@ game = {total_kills: 0,
   end
 end
 
-puts "\n\ngame: total_kills= #{game[:total_kills]}\n #{game[:players]}"
+#puts "\n\ngame: total_kills= #{game[:total_kills]}\n #{game[:players]}"
+puts "\n\ngame:", game
 
 
 data.close
