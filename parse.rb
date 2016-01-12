e4425bd7dea5a53e649985a3515ef5335a279f8c
SeuRAUL <kenny.is.inmortal@gmail.com>
2016-01-12 11:15:20 -0300

    find line with 'Kill:'

--- parse.rb
+++ parse.rb
@@ -1,3 +1,13 @@
 logfile = ARGV.first
 
 data = File.open(logfile)
+
+# data.each do
+(1..100).each do
+  kill = data.readline.split(' ')
+
+  puts "#{kill[5]} #{kill[7]} #{kill[9]}" if kill.include? "Kill:"
+end
+
+
+data.close
